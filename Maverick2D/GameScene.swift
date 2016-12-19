cc409e86dac7aba1c682fcb865912be125440d94
Alex DeMars <alexdemars@gmail.com>
2016-12-19 15:21:12 -0800

    initial commit

--- Maverick2D/GameScene.swift
+++ Maverick2D/GameScene.swift
@@ -10,80 +10,42 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+  let zero: SKSpriteNode = {
+    let sprite = SKSpriteNode(imageNamed: "zero")
+    sprite.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    sprite.size = CGSize(width: 128, height: 128)
+    sprite.physicsBody?.affectedByGravity = false
+    sprite.zPosition = 10
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    return sprite
+  }()
+  
+  override func didMove(to view: SKView) {
+    createMap()
+    self.addChild(zero)
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+  
+  func createMap() {
+    let map = SKSpriteNode(imageNamed: "map")
+    map.name = "map"
+    map.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    map.position = CGPoint(x: 0, y: 0)
+    map.size = CGSize(width: 4096, height: 4096)
+    map.physicsBody?.affectedByGravity = false
+    map.zPosition = 1
+    self.addChild(map)
+  }
+  
+  func moveMap() {
+  
+  }
 }
