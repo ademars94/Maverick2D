bfe27c33d5942ce3dc0bddd7eebc466e4e2a4a48
Alex DeMars <alexdemars@gmail.com>
2016-12-21 12:15:37 -0800

    player can control plane

--- Maverick2D/GameScene.swift
+++ Maverick2D/GameScene.swift
@@ -11,7 +11,11 @@ import GameplayKit
 
 class GameScene: SKScene {
   
-  var player = Player(x: 0, y: 0, angle: 0, speed: 10, plane: Plane(type: "spitfire"))
+  var player = Player(x: 0, y: 0, angle: 0, speed: 7, plane: Plane(type: "spitfire"))
+  var lastUpdatedTime: TimeInterval = 0.0
+  
+  var isTurningRight = false
+  var isTurningLeft = false
   
   let map: SKSpriteNode = {
     let sprite = SKSpriteNode(imageNamed: "map")
@@ -32,15 +36,60 @@ class GameScene: SKScene {
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     for touch in touches {
       if touch.location(in: self).x < 0 {
-        player.angle += 10
+        isTurningRight = false
+        isTurningLeft = true
       }
       if touch.location(in: self).x > 0 {
-        player.angle -= 10
+        isTurningLeft = false
+        isTurningRight = true
+      }
+    }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for touch in touches {
+      if touch.location(in: self).x < 0 {
+        isTurningRight = false
+        isTurningLeft = true
+      }
+      if touch.location(in: self).x > 0 {
+        isTurningLeft = false
+        isTurningRight = true
+      }
+    }
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for touch in touches {
+      if touch.location(in: self).x < 0 {
+        isTurningLeft = false
+      }
+      if touch.location(in: self).x > 0 {
+        isTurningRight = false
       }
     }
   }
   
   override func update(_ currentTime: TimeInterval) {
+    if lastUpdatedTime == 0.0 {
+      lastUpdatedTime = currentTime
+    }
+    
+    let delta = currentTime - lastUpdatedTime
+    print("Delta: \(delta)")
+    
+    var accumulatedFrames = round((currentTime - lastUpdatedTime) * 60)
+    
+    lastUpdatedTime = currentTime
+    
+    while accumulatedFrames > 0 {
+      print("\(accumulatedFrames) accumulated")
+      updateWorld()
+      accumulatedFrames -= 1
+    }
+  }
+  
+  func updateWorld() {
     movePlane()
   }
   
@@ -54,6 +103,12 @@ class GameScene: SKScene {
     
     if dy < 2048 && dy > -2048 {
       player.y = dy
+    }
+    
+    if isTurningLeft {
+      player.angle += 3
+    } else if isTurningRight {
+      player.angle -= 3
     }
     
     map.physicsBody?.velocity = CGVector(dx: dx, dy: dy)
